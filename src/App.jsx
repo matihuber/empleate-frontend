4c0afcbddfd539854c08c7c768e58c5298c0fa94
matihuber <matiashuber94@gmail.com>
2025-07-16 21:31:41 -0300

    Inicializando Empleate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Empleate
+      </h1>
+    </div>
+  )
+}
+export default App;
